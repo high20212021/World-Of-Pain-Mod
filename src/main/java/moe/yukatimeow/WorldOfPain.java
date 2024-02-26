205f67e5bd20d5f5623bd4a7119a05e872de6f9c
Yukati2024 <high20212021@outlook.com>
2024-02-26 19:03:41 +0800

    add world killer

--- src/main/java/moe/yukatimeow/WorldOfPain.java
+++ src/main/java/moe/yukatimeow/WorldOfPain.java
@@ -3,7 +3,9 @@ package moe.yukatimeow;
 import moe.yukatimeow.worldofpain.items.UnstablePower;
 import moe.yukatimeow.worldofpain.items.WorldPoint;
 import moe.yukatimeow.worldofpain.tools.weapon.UnstablePowerSword;
+import moe.yukatimeow.worldofpain.tools.weapon.WorldSword;
 import moe.yukatimeow.worldofpain.tools.UnstablePowerToolMaterial;
+import moe.yukatimeow.worldofpain.tools.WorldToolMaterial;
 import moe.yukatimeow.worldofpain.tools.UnstablePowerAxe;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
@@ -26,6 +28,7 @@ public class WorldOfPain implements ModInitializer {
 
   public static final UnstablePower UNSTABLE_POWER = new UnstablePower(new Item.Settings().group(ItemGroup.MISC).maxCount(16).fireproof());
   public static final UnstablePowerSword UNSTABLE_POWER_SWORD = new UnstablePowerSword(new UnstablePowerToolMaterial(), 14, -0.08f, new Item.Settings().group(ItemGroup.COMBAT));
+  public static final WorldSword WORLDSWORD = new WorldSword(new WorldToolMaterial(), 2147483647, -0.10f, new Item.Settings().group(ItemGroup.COMBAT));
   public static final WorldPoint WORLDPOINT = new WorldPoint(new Item.Settings().group(ItemGroup.MISC).maxCount(1));
   public static final UnstablePowerAxe UNSTABLE_POWER_AXE = new UnstablePowerAxe(new UnstablePowerToolMaterial(), 16, -0.08f, new Item.Settings().group(ItemGroup.TOOLS));
   public static final Item HORSE_MEAT = new Item(new Item.Settings().group(ItemGroup.FOOD).food(new FoodComponent.Builder()
@@ -44,6 +47,7 @@ public class WorldOfPain implements ModInitializer {
     Registry.register(Registry.ITEM,new Identifier("worldofpain", "unstable_power_axe"),UNSTABLE_POWER_AXE);
     Registry.register(Registry.ITEM,new Identifier("worldofpain", "horse_meat"),HORSE_MEAT);
     FuelRegistry.INSTANCE.add(WorldOfPain.UNSTABLE_POWER, 1310700);
+    Registry.register(Registry.ITEM,new Identifier("worldofpain", "worldsword"),WORLDSWORD);
   
   }
 }
---
+++ src/main/java/moe/yukatimeow/tools/weapon/WorldSword.java
@@ -0,0 +1,10 @@
+package moe.yukatimeow.worldofpain.tools.weapon;
+
+import net.minecraft.item.SwordItem;
+import net.minecraft.item.ToolMaterial;
+
+public class WorldSword extends SwordItem {
+    public WorldSword(ToolMaterial toolMaterial, int attackDamage, float attackSpeed, Settings settings) {
+	super(toolMaterial, attackDamage, attackSpeed, settings);
+    }
+}
