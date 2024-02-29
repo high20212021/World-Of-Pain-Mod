4452329840d62cf98a745b158f792793842c6fe9
Yukati2024 <high20212021@outlook.com>
2024-02-29 23:14:11 +0800

    add explosion to unstablepowersword

--- src/main/java/moe/yukatimeow/tools/weapon/UnstablePowerSword.java
+++ src/main/java/moe/yukatimeow/tools/weapon/UnstablePowerSword.java
@@ -2,9 +2,21 @@ package moe.yukatimeow.worldofpain.tools.weapon;
 
 import net.minecraft.item.SwordItem;
 import net.minecraft.item.ToolMaterial;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.world.World;
+import net.minecraft.world.explosion.Explosion;
 
 public class UnstablePowerSword extends SwordItem {
-    public UnstablePowerSword(ToolMaterial toolMaterial, int attackDamage, float attackSpeed, Settings settings) {
-	super(toolMaterial, attackDamage, attackSpeed, settings);
-    }
+  public UnstablePowerSword(ToolMaterial toolMaterial, int attackDamage, float attackSpeed, Settings settings) {
+	  super(toolMaterial, attackDamage, attackSpeed, settings);
+  }
+  
+  @Override
+  public boolean postHit(ItemStack stack, LivingEntity target, LivingEntity attacker){
+    World world = attacker.getWorld();
+    world.createExplosion(attacker, attacker.getX(), attacker.getY(), attacker.getZ(), 5, false, Explosion.DestructionType.DESTROY);
+    return super.postHit(stack, target, attacker);
+  }
 }
